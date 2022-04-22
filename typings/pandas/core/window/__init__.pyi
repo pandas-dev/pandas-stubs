ba7aa5f0d5f629904c3a6d4030fd484b0cdb8047
Irv Lustig <irv@princeton.com>
2022-04-22 18:00:20 -0400

    Copy from microsoft/python-type-stubs

---
+++ typings/pandas/core/window/__init__.pyi
@@ -0,0 +1,3 @@
+from .ewm import ExponentialMovingWindow as ExponentialMovingWindow
+from .expanding import Expanding as Expanding, ExpandingGroupby as ExpandingGroupby
+from .rolling import Rolling as Rolling, RollingGroupby as RollingGroupby, Window as Window
