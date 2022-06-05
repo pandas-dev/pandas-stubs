7176fd0c86fa688871a273c400018284aa8357ab
Breno Fernandes <brenojesusfernandes@pm.com>
2022-06-05 21:02:32 -0230

    Feature: Add a script to run all tests, just type "poetry run all_test"

---
+++ scripts/tests.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def test_all():
+
+    cmd = ['mypy', 'pandas-stubs', 'tests']
+    subprocess.run(cmd)
+
+    cmd = ['pytest']
+    subprocess.run(cmd)
+
+    cmd = ['pyright']
+    subprocess.run(cmd)
