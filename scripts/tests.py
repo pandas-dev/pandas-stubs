34ec92ae11cb9792d4390a982fe82a3532ea19ca
Breno Fernandes <brenojesusfernandes@pm.com>
2022-06-08 15:08:06 -0230

    Fix: Add tests with wheel

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,13 +1,41 @@
 import subprocess
+from pathlib import Path
 
 
-def test_all():
-
-    cmd = ['mypy', 'pandas-stubs', 'tests']
+def __test_all():
+    cmd = ["mypy", "pandas-stubs", "tests"]
     subprocess.run(cmd)
 
-    cmd = ['pytest']
+    cmd = ["pytest"]
     subprocess.run(cmd)
 
-    cmd = ['pyright']
+    cmd = ["pyright"]
     subprocess.run(cmd)
+
+
+def install_wheel():
+    cmd = ["poetry", "build", "-f", "wheel"]
+    subprocess.run(cmd)
+
+    path = next(Path("dist/").glob("*.whl"))
+    cmd = ["pip", "install", str(path)]
+    subprocess.run(cmd)
+
+
+def __clean_env():
+    cmd = ["pip", "uninstall", "-y", "pandas-stubs"]
+    subprocess.run(cmd)
+
+    cmd = ["poetry", "install"]
+    subprocess.run(cmd)
+
+
+def run_all():
+
+    __test_all()
+
+    install_wheel()
+
+    __test_all()
+
+    __clean_env()
