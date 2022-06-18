2107b980494d6686ca165a88d36bb3b8e8e074d6
Breno Fernandes (Mac OS) <brenojesusfernandes@pm.me>
2022-06-18 15:37:54 -0400

    Feature: Add util script to build the project

---
+++ scripts/build.py
@@ -0,0 +1,9 @@
+import subprocess
+
+def run_build():
+    # Update project version
+    cmd = ["poetry-dynamic-versioning"]
+    subprocess.run(cmd, check=True)
+
+    cmd = ["poetry", "build"]
+    subprocess.run(cmd, check=True)
