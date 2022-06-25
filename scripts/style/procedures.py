571f409c4b335f22c25e39a975a039af4f83e6db
Breno Fernandes (Mac OS) <brenojesusfernandes@pm.me>
2022-06-25 10:00:09 -0400

    Fix: Add style check and format to util scripts folder

--- scripts/style/procedures.py
+++ scripts/style/procedures.py
@@ -2,20 +2,20 @@ import subprocess
 
 
 def run_black_check():
-    cmd = ["black", "--check", "pandas-stubs", "tests"]
+    cmd = ["black", "--check", "pandas-stubs", "tests", "scripts"]
     subprocess.run(cmd, check=True)
 
 
 def run_isort_check():
-    cmd = ["isort", "--check-only", "pandas-stubs", "tests"]
+    cmd = ["isort", "--check-only", "pandas-stubs", "tests", "scripts"]
     subprocess.run(cmd, check=True)
 
 
 def run_format_black():
-    cmd = ["black", "pandas-stubs", "tests"]
+    cmd = ["black", "pandas-stubs", "tests", "scripts"]
     subprocess.run(cmd, check=True)
 
 
 def run_format_isort():
-    cmd = ["isort", "pandas-stubs", "tests"]
+    cmd = ["isort", "pandas-stubs", "tests", "scripts"]
     subprocess.run(cmd, check=True)
