e95cbddd93f30cb4bcc0c829dfacae69b1962f21
Torsten Wörtwein <twoertwein@gmail.com>
2022-07-04 09:48:39 -0400

    pyupgrade should fail on main

--- scripts/style/run.py
+++ scripts/style/run.py
@@ -16,9 +16,18 @@ def check_isort():
 
 def check_pyupgrade():
     cmd = ["pyupgrade", "--py38-plus", "--keep-runtime-typing"]
+    success = True
     for folder in CHECK_FOLDERS:
-        for py_file in Path(folder).glob("**/*.[py][pyi]"):
-            subprocess.run(cmd + [str(py_file)], check=True)
+        for py_file in Path(folder).glob("**/*.py*"):
+            if py_file.suffix not in (".py", ".pyi"):
+                continue
+            try:
+                subprocess.run(cmd + [str(py_file)], check=True)
+            except subprocess.CalledProcessError:
+                success = False
+    if not success:
+         subprocess.run(["git", "diff"], check=True)
+         raise RuntimeError("pyupgrade failed")
 
 
 def format_black():
@@ -39,5 +48,7 @@ def format_pyupgrade():
         "--exit-zero-even-if-changed",
     ]
     for folder in CHECK_FOLDERS:
-        for py_file in Path(folder).glob("**/*.[py][pyi]"):
+        for py_file in Path(folder).glob("**/*.py*"):
+            if py_file.suffix not in (".py", ".pyi"):
+                continue
             subprocess.run(cmd + [str(py_file)], check=True)
