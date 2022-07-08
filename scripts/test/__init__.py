06ed75e0612416b34a092deb6840362cd63d1916
Torsten Wörtwein <twoertwein@gmail.com>
2022-07-08 18:01:10 -0400

    added _step.mypy_src back

--- scripts/test/__init__.py
+++ scripts/test/__init__.py
@@ -17,7 +17,7 @@ def test_src(profile: str, clean_cache: bool = False):
     elif profile == "pytest":
         steps.extend([_step.pytest_src])
     elif profile == "full":
-        steps.extend([_step.pyright_src, _step.pytest_src])
+        steps.extend([_step.mypy_src, _step.pyright_src, _step.pytest_src])
     else:
         raise ModuleNotFoundError("Profile not found!")
 
@@ -61,6 +61,7 @@ def test_all(clean_cache: bool = False):
 
     steps.extend(
         [
+            _step.mypy_src,
             _step.pyright_src,
             _step.pytest_src,
             _step.build_dist,
