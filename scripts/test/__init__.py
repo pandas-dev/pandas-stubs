29cfa27a61ff1264e23df532ae5f255421bc9242
Torsten Wörtwein <twoertwein@gmail.com>
2022-07-13 17:04:31 -0400

    remove options from test_src

--- scripts/test/__init__.py
+++ scripts/test/__init__.py
@@ -1,77 +1,28 @@
 from scripts._job import run_job
 from scripts.test import _step
 
+_CACHE_STEPS = [_step.clean_mypy_cache, _step.clean_pytest_cache]
+_SRC_STEPS = [_step.mypy_src, _step.pyright_src, _step.pytest_src, _step.style_src]
+_DIST_STEPS = [
+    _step.build_dist,
+    _step.install_dist,
+    _step.rename_src,
+    _step.mypy_dist,
+    _step.pyright_dist,
+    _step.uninstall_dist,
+    _step.restore_src,
+]
 
-def test_src(profile: str, clean_cache: bool = False):
+
+def test(clean_cache: bool = False, src: bool = False, dist: bool = False):
     steps = []
     if clean_cache:
-        steps.extend(
-            [
-                _step.clean_mypy_cache,
-                _step.clean_pytest_cache,
-            ]
-        )
+        steps.extend(_CACHE_STEPS)
 
-    if profile in (None, "", "typing"):
-        steps.extend([_step.mypy_src, _step.pyright_src])
-    elif profile == "pytest":
-        steps.extend([_step.pytest_src])
-    elif profile == "style":
-        steps.extend([_step.style_src])
-    elif profile == "all":
-        steps.extend(
-            [_step.mypy_src, _step.pyright_src, _step.pytest_src, _step.style_src]
-        )
-    else:
-        raise ModuleNotFoundError("Profile not found!")
+    if src:
+        steps.extend(_SRC_STEPS)
 
-    run_job(steps)
-
-
-def test_dist(clean_cache: bool = False):
-    steps = []
-    if clean_cache:
-        steps.extend([_step.clean_mypy_cache])
-
-    steps.extend(
-        [
-            _step.build_dist,
-            _step.install_dist,
-            _step.rename_src,
-            _step.mypy_dist,
-            _step.pyright_dist,
-            _step.uninstall_dist,
-            _step.restore_src,
-        ]
-    )
-
-    run_job(steps)
-
-
-def test_all(clean_cache: bool = False):
-    steps = []
-    if clean_cache:
-        steps.extend(
-            [
-                _step.clean_mypy_cache,
-                _step.clean_pytest_cache,
-            ]
-        )
-
-    steps.extend(
-        [
-            _step.mypy_src,
-            _step.pyright_src,
-            _step.pytest_src,
-            _step.style_src,
-            _step.build_dist,
-            _step.install_dist,
-            _step.rename_src,
-            _step.mypy_dist,
-            _step.pyright_dist,
-            _step.uninstall_dist,
-            _step.restore_src,
-        ]
-    )
+    if src:
+        steps.extend(_DIST_STEPS)
 
     run_job(steps)
