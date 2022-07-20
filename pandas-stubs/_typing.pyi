e7e80d1a5f08480ea6b0cedb3934ee72a2387325
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-07-20 13:41:34 +0100

    MAINT: Revert npt export method

--- pandas-stubs/_typing.pyi
+++ pandas-stubs/_typing.pyi
@@ -27,7 +27,7 @@ from typing import (
 )
 
 import numpy as np
-import numpy.typing
+from numpy import typing as npt
 from pandas.core.arrays import ExtensionArray
 from pandas.core.frame import DataFrame
 from pandas.core.generic import NDFrame
@@ -41,8 +41,6 @@ from pandas._libs.tslibs import (
 )
 
 from pandas.core.dtypes.dtypes import ExtensionDtype
-
-npt = numpy.typing
 
 ArrayLike = Union[ExtensionArray, np.ndarray]
 AnyArrayLike = Union[Index, Series, np.ndarray]
