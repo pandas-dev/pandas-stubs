2e254677854095f42535f91c60b736393e190a9f
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-07-22 18:58:41 +0100

    ENH: Add read_table
    Fully add read table and supporting classes

--- pandas-stubs/_typing.pyi
+++ pandas-stubs/_typing.pyi
@@ -16,6 +16,7 @@ from typing import (
     AnyStr,
     Callable,
     Hashable,
+    Iterator,
     Literal,
     Mapping,
     Optional,
@@ -69,6 +70,15 @@ class WriteBuffer(BaseBuffer, Protocol[AnyStr_cov]): ...
 
 class ReadPickleBuffer(ReadBuffer[bytes], Protocol):
     def readline(self) -> AnyStr_cov: ...
+
+class WriteExcelBuffer(WriteBuffer[bytes], Protocol):
+    def truncate(self, size: int | None = ...) -> int: ...
+
+class ReadCsvBuffer(ReadBuffer[AnyStr_cov], Protocol[AnyStr_cov]):
+    def __iter__(self) -> Iterator[AnyStr_cov]: ...
+    def readline(self) -> AnyStr_cov: ...
+    @property
+    def closed(self) -> bool: ...
 
 FilePath = Union[str, PathLike[str]]
 
@@ -210,5 +220,7 @@ GroupByObjectNonScalar = Union[
     tuple, list[Label], Function, Series, np.ndarray, Mapping[Label, Any], Index
 ]
 GroupByObject = Union[Scalar, GroupByObjectNonScalar]
+
+CSVEngine = Literal["c", "python", "pyarrow", "python-fwf"]
 
 __all__ = ["npt", "type_t"]
