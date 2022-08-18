03c8f98393612032575ab5473a4e949cc93d56da
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-08-18 08:27:55 +0100

    TST: Make test skippable if not available

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,13 +1,18 @@
 from pandas import DataFrame
+import pytest
 from typing_extensions import assert_type
 
 from tests import check
 
+from pandas.io.clipboard import PyperclipException
 from pandas.io.clipboards import read_clipboard
 
 DF = DataFrame({"a": [1, 2, 3], "b": [0.0, 0.0, 0.0]})
 
 
 def test_clipboard():
-    DF.to_clipboard()
+    try:
+        DF.to_clipboard()
+    except PyperclipException:
+        pytest.skip("clipboard not available for testing")
     check(assert_type(read_clipboard(), DataFrame), DataFrame)
