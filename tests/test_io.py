417c3d0f8301e2eee0fc75d492f9f1eb74d2bca5
Kevin Sheppard <bashtage@users.noreply.github.com>
2022-08-22 17:30:48 +0100

    ENH: Synchronize XML (#207)
    * ENH: Synchronize XML
    
    Synchronize to and read XML to upstream
    
    * TST: Add tests for read_xml and to_xml
    
    * MAINT: Cap lxml
    
    * ENH: Improve specificity of types
    
    Remove erroneous Nones

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,8 +1,11 @@
+import io
+
 import pandas as pd
 from pandas import (
     DataFrame,
     read_clipboard,
     read_stata,
+    read_xml,
 )
 from pandas._testing import ensure_clean
 import pytest
@@ -15,6 +18,21 @@ from pandas.io.parsers import TextFileReader
 from pandas.io.stata import StataReader
 
 DF = DataFrame({"a": [1, 2, 3], "b": [0.0, 0.0, 0.0]})
+
+
+def test_xml():
+    with ensure_clean() as path:
+        check(assert_type(DF.to_xml(path), None), type(None))
+        check(assert_type(read_xml(path), DataFrame), DataFrame)
+        with open(path, "rb") as f:
+            check(assert_type(read_xml(f), DataFrame), DataFrame)
+
+
+def test_xml_str():
+    with ensure_clean() as path:
+        check(assert_type(DF.to_xml(), str), str)
+        out: str = DF.to_xml()
+        check(assert_type(read_xml(io.StringIO(out)), DataFrame), DataFrame)
 
 
 def test_read_stata_df():
