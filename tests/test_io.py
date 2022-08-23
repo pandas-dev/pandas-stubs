58cd5a887aa84eb38c8a8555c6975ed36e2e4cb0
Kevin Sheppard <bashtage@users.noreply.github.com>
2022-08-23 13:10:46 +0100

    ENH: Synchronize ORC with upstream (#204)
    * ENH: Synchronie ORC with upstream
    
    Synchronize the orc read and write functions with upstream pandas
    
    * ENH: Move to_orc to correct location
    
    Remove standalone
    Add to frame
    
    * TST: Add tests for ORC
    
    Add tests that will run against pandas 1.5.0

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,9 +1,13 @@
 import io
+from pathlib import Path
 
+from packaging.version import parse
 import pandas as pd
 from pandas import (
     DataFrame,
+    __version__,
     read_clipboard,
+    read_orc,
     read_stata,
     read_xml,
 )
@@ -18,6 +22,47 @@ from pandas.io.parsers import TextFileReader
 from pandas.io.stata import StataReader
 
 DF = DataFrame({"a": [1, 2, 3], "b": [0.0, 0.0, 0.0]})
+
+PD_LT_15 = parse(__version__) < parse("1.5.0")
+
+
+@pytest.mark.skipif(PD_LT_15, reason="pandas 1.5.0 or later required")
+def test_orc():
+    with ensure_clean() as path:
+        check(assert_type(DF.to_orc(path), None), type(None))
+        check(assert_type(read_orc(path), DataFrame), DataFrame)
+
+
+@pytest.mark.skipif(PD_LT_15, reason="pandas 1.5.0 or later required")
+def test_orc_path():
+    with ensure_clean() as path:
+        pathlib_path = Path(path)
+        check(assert_type(DF.to_orc(pathlib_path), None), type(None))
+        check(assert_type(read_orc(pathlib_path), DataFrame), DataFrame)
+
+
+@pytest.mark.skipif(PD_LT_15, reason="pandas 1.5.0 or later required")
+def test_orc_buffer():
+    with ensure_clean() as path:
+        file_w = open(path, "wb")
+        check(assert_type(DF.to_orc(file_w), None), type(None))
+        file_w.close()
+
+        file_r = open(path, "rb")
+        check(assert_type(read_orc(file_r), DataFrame), DataFrame)
+        file_r.close()
+
+
+@pytest.mark.skipif(PD_LT_15, reason="pandas 1.5.0 or later required")
+def test_orc_columns():
+    with ensure_clean() as path:
+        check(assert_type(DF.to_orc(path, index=False), None), type(None))
+        check(assert_type(read_orc(path, columns=["a"]), DataFrame), DataFrame)
+
+
+@pytest.mark.skipif(PD_LT_15, reason="pandas 1.5.0 or later required")
+def test_orc_bytes():
+    check(assert_type(DF.to_orc(index=False), bytes), bytes)
 
 
 def test_xml():
