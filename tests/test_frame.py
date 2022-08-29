e477f0d36081cb5e406e3a0b6dee1f5205560f15
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-08-29 09:00:33 +0200

    Adjust

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1414,6 +1414,8 @@ def test_set_columns() -> None:
     df.columns = pd.Series([1, 2])  # type: ignore[assignment]
     df.columns = np.array([1, "a"])  # type: ignore[assignment]
     df.columns = pd.Series([1, "a"])  # type: ignore[assignment]
+    df.columns = (1, 2)  # type: ignore[assignment]
+    df.columns = (1, "a")  # type: ignore[assignment]
 
 
 def test_frame_index_numpy() -> None:
