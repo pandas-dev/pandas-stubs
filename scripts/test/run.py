299b8d42833f5ef4c3255a7631c25b49a0e1b429
Irv Lustig <irv@princeton.com>
2022-08-31 20:32:24 -0400

    print sys.executable

--- scripts/test/run.py
+++ scripts/test/run.py
@@ -5,6 +5,7 @@ import sys
 
 def mypy_src():
     # cmd = ["mypy", "pandas-stubs", "tests", "--no-incremental"]
+    print("sys.executable ", sys.executable)
     cmd = [sys.executable, "testing.py"]
     subprocess.run(cmd, check=True)
 
