3cb14af09574d8bda3a51f1a63002e1443f2f7e3
Kevin Sheppard <bashtage@users.noreply.github.com>
2022-09-17 01:14:26 +0100

    BUG: Generalize options (#306)
    * BUG: Generalize options
    
    Generalize using specific classes for each level
    
    * TST: Correct backend
    
    Co-authored-by: Kevin Sheppard <kevin.sheppard@gmail.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,6 @@
 from typing import (
+    TYPE_CHECKING,
     Any,
-    Union,
 )
 
 import pandas as pd
@@ -10,17 +10,29 @@ from pandas._config.config import DictWrapper
 
 from tests import check
 
+if TYPE_CHECKING:
+    from pandas._config.config import (
+        Display,
+        Options,
+    )
+else:
+    Display = Options = Any
+
 
 def test_option_tools():
     check(assert_type(pd.reset_option("display.width"), None), type(None))
     check(assert_type(pd.set_option("display.width", 80), None), type(None))
     check(assert_type(pd.describe_option("display.width", False), str), str)
     check(assert_type(pd.describe_option("display.width", True), None), type(None))
-    check(assert_type(pd.options, DictWrapper), DictWrapper)
-    check(
-        assert_type(pd.options.display, Union[str, bool, int, None, DictWrapper]),
-        DictWrapper,
-    )
+    check(assert_type(pd.options, Options), DictWrapper)
+    check(assert_type(pd.options.display, Display), DictWrapper)
     check(assert_type(pd.get_option("display.width"), Any), int)
     with pd.option_context("display.width", 120):
         assert pd.get_option("display.width") == 120
+
+
+def test_specific_option():
+    # GH 294
+    check(assert_type(pd.options.plotting.backend, str), str)
+    # Just check assignment
+    pd.options.plotting.backend = "matplotlib"
