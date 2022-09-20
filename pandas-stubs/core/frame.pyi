43fdc064c90250b3fd319b8b1c045585f650b9a1
Torsten Wörtwein <twoertwein@gmail.com>
2022-09-20 19:00:53 -0400

    fix some keyword-only arguments

--- pandas-stubs/core/frame.pyi
+++ pandas-stubs/core/frame.pyi
@@ -1787,6 +1787,7 @@ class DataFrame(NDFrame, OpsMixin):
         min_periods: int | None = ...,
         center: _bool = ...,
         *,
+        win_type: None = ...,
         on: Hashable | None = ...,
         axis: AxisType = ...,
         closed: IntervalClosedType | None = ...,
