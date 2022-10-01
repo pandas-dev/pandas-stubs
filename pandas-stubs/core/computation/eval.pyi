511876d2d064f25cb6021697f8cba87e11527411
Kevin Sheppard <bashtage@users.noreply.github.com>
2022-10-01 01:37:36 +0100

    ENH: Improve hashing and eval (#347)
    * ENH: Improve hash functions
    
    * ENH: Improve eval

--- pandas-stubs/core/computation/eval.pyi
+++ pandas-stubs/core/computation/eval.pyi
@@ -1,12 +1,30 @@
+from typing import (
+    Any,
+    Literal,
+    Mapping,
+)
+
+from pandas import (
+    DataFrame,
+    Series,
+)
+from pandas.core.computation.ops import BinOp
+
+from pandas._typing import (
+    Scalar,
+    npt,
+)
+
 def eval(
-    expr,
-    parser=...,
-    engine: str | None = ...,
-    truediv=...,
-    local_dict=...,
-    global_dict=...,
-    resolvers=...,
-    level=...,
-    target=...,
-    inplace=...,
-): ...
+    expr: str | BinOp,
+    parser: Literal["pandas", "python"] = ...,
+    engine: Literal["python", "numexpr"] | None = ...,
+    # Keyword only due to omitted deprecated argument
+    *,
+    local_dict: dict[str, Any] | None = ...,
+    global_dict: dict[str, Any] | None = ...,
+    resolvers: list[Mapping] | None = ...,
+    level: int = ...,
+    target: object | None = ...,
+    inplace: bool = ...,
+) -> npt.NDArray | Scalar | DataFrame | Series | None: ...
