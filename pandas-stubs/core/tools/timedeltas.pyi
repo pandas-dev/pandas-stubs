d360eb638b54b6851804ee3227e026116cf35aad
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-10-03 07:39:33 +0100

    TYP: Improve to_timedelta

--- pandas-stubs/core/tools/timedeltas.pyi
+++ pandas-stubs/core/tools/timedeltas.pyi
@@ -1,6 +1,8 @@
-# def to_timedelta(arg, unit: str = ..., errors: str = ...): ...
 from datetime import timedelta
-from typing import overload
+from typing import (
+    Sequence,
+    overload,
+)
 
 from pandas import Index
 from pandas.core.indexes.timedeltas import TimedeltaIndex
@@ -16,11 +18,10 @@ from pandas._typing import (
     DateTimeErrorChoices,
 )
 
-# Copied from pandas/_libs/tslibs/timedeltas.pyx
-
 @overload
 def to_timedelta(
     arg: str | float | timedelta,
+    # TODO: Check all UnitChoices are valid
     unit: UnitChoices | None = ...,
     errors: DateTimeErrorChoices = ...,
 ) -> Timedelta: ...
@@ -32,7 +33,12 @@ def to_timedelta(
 ) -> TimedeltaSeries: ...
 @overload
 def to_timedelta(
-    arg: list | tuple | range | ArrayLike | Index,
+    arg: Sequence[float | timedelta]
+    | list[str | float | timedelta]
+    | tuple[str | float | timedelta, ...]
+    | range
+    | ArrayLike
+    | Index,
     unit: UnitChoices | None = ...,
     errors: DateTimeErrorChoices = ...,
 ) -> TimedeltaIndex: ...
