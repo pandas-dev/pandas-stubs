c395a33d605cfeef54abd1915f4302ba4a0d6b10
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-10-08 08:04:13 +0100

    ENH: Improve typing for pivot and pivot_table

--- pandas-stubs/core/reshape/pivot.pyi
+++ pandas-stubs/core/reshape/pivot.pyi
@@ -1,36 +1,51 @@
 from typing import (
     Callable,
+    Hashable,
     Literal,
     Sequence,
+    TypeAlias,
+    TypeVar,
 )
 
+import numpy as np
 from pandas.core.frame import DataFrame
 from pandas.core.groupby.grouper import Grouper
 from pandas.core.series import Series
 
 from pandas._typing import (
-    IndexLabel,
+    Label,
     Scalar,
 )
 
+_HashableT1 = TypeVar("_HashableT1", bound=Hashable)
+_HashableT2 = TypeVar("_HashableT2", bound=Hashable)
+_HashableT3 = TypeVar("_HashableT3", bound=Hashable)
+
+_PivotAggFunc: TypeAlias = Callable[[Series], Scalar]
+
 def pivot_table(
     data: DataFrame,
-    values: str | None = ...,
-    index: str | Sequence | Grouper | None = ...,
-    columns: str | Sequence | Grouper | None = ...,
-    aggfunc=...,
+    values: Label | None = ...,
+    index: Label | list[_HashableT1] | Grouper | None = ...,
+    columns: Label | list[_HashableT2] | Grouper | None = ...,
+    aggfunc: _PivotAggFunc
+    | list[_PivotAggFunc]
+    | dict[_HashableT3, _PivotAggFunc]
+    | Literal["mean", "sum", "count", "min", "max", "median", "std", "var"]
+    | np.ufunc = ...,
     fill_value: Scalar | None = ...,
     margins: bool = ...,
     dropna: bool = ...,
     margins_name: str = ...,
     observed: bool = ...,
+    sort: bool = ...,
 ) -> DataFrame: ...
 def pivot(
     data: DataFrame,
     *,
-    index: IndexLabel = ...,
-    columns: IndexLabel = ...,
-    values: IndexLabel = ...,
+    index: Hashable | list[_HashableT1] = ...,
+    columns: Hashable | list[_HashableT2] = ...,
+    values: Hashable | list[_HashableT3] = ...,
 ) -> DataFrame: ...
 def crosstab(
     index: Sequence | Series,
