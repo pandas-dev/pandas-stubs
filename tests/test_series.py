175e9f1f425d94e40d963abc00c0dd3693dac770
ram vikram singh <ramvikrams243@gmail.com>
2022-10-08 23:47:34 +0530

    minute changes
    added or and xor operator

--- tests/test_series.py
+++ tests/test_series.py
@@ -1243,6 +1243,12 @@ def test_bitwise_operators() -> None:
     # for issue #348 (bitwise operators on Series should support int)
     check(assret_type(s & 3, "pd.Series[int]"), pd.Series,int)
     check(assert_type(3 & s, "pd.Series[int]"), pd.Series,int)
+    
+    check(assret_type(s | 3, "pd.Series[int]"), pd.Series,int)
+    check(assert_type(3 | s, "pd.Series[int]"), pd.Series,int)
+    
+    check(assret_type(s ^ 3, "pd.Series[int]"), pd.Series,int)
+    check(assert_type(3 ^ s, "pd.Series[int]"), pd.Series,int)
 
     check(assert_type(s & [1, 2, 3, 4], "pd.Series[int]"), pd.Series, int)
     check(assert_type([1, 2, 3, 4] & s, "pd.Series[int]"), pd.Series, int)
