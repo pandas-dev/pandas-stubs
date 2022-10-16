d1d767502a7089c313cff6a0e942354380035ba7
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2022-10-16 22:09:31 +0100

    ENH: IMprove boolean array

--- pandas-stubs/core/arrays/boolean.pyi
+++ pandas-stubs/core/arrays/boolean.pyi
@@ -1,8 +1,17 @@
+from typing import (
+    Sequence,
+    Union,
+    overload,
+)
+
 import numpy as np
 from pandas.core.arrays import ExtensionArray
+from typing_extensions import TypeAlias
 
+from pandas._libs.missing import NAType
 from pandas._typing import (
     Scalar,
+    npt,
     type_t,
 )
 
@@ -22,11 +31,29 @@ class BooleanDtype(ExtensionDtype):
     def construct_array_type(cls) -> type_t[BooleanArray]: ...
     def __from_arrow__(self, array): ...
 
+_ScalarType: TypeAlias = Union[bool, np.bool_, NAType, None]
+_ArrayKey: TypeAlias = Union[Sequence[int], npt.NDArray[np.integer], slice]
+
 class BooleanArray(BaseMaskedArray):
     def __init__(
-        self, values: np.ndarray, mask: np.ndarray, copy: bool = ...
+        self,
+        values: npt.NDArray[np.bool_],
+        mask: npt.NDArray[np.bool_],
+        copy: bool = ...,
     ) -> None: ...
-    def __setitem__(self, key: int | np.ndarray | slice, value: object) -> None: ...
+    # Ignore overrides since more specific than super type
+    @overload  # type: ignore[override]
+    def __setitem__(self, key: int, value: _ScalarType) -> None: ...
+    @overload
+    def __setitem__(
+        self,
+        key: _ArrayKey,
+        value: _ScalarType | Sequence[bool | NAType | None] | npt.NDArray[np.bool_],
+    ) -> None: ...
+    @overload  # type: ignore[override]
+    def __getitem__(self, item: int) -> bool | NAType: ...
+    @overload
+    def __getitem__(self, item: _ArrayKey) -> BooleanArray: ...
     @property
     def dtype(self): ...
     def astype(
