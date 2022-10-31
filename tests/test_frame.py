e61a894c4a8416b35beb5f4e270cf80bf156d989
ram vikram singh <ramvikrams243@gmail.com>
2022-11-01 02:38:56 +0530

    update

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -1917,7 +1917,7 @@ def test_resample_150_changes() -> None:
     check(assert_type(resampler.apply(f), Union[pd.Series, pd.DataFrame]), pd.DataFrame)
 
 
-def df_accepting_dicts_iterator() -> None: 
+def df_accepting_dicts_iterator() -> None:
     # GH 392
     data = [{"a": 1, "b": 2}, {"a": 3, "b": 5}]
     check(assert_type(pd.DataFrame(iter(data)), pd.DataFrame), pd.DataFrame)
