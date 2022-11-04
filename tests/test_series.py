8b5466bc119c8b703547431e89184112c02bb61e
Kevin Sheppard <kevin.sheppard@gmail.com>
2022-11-04 07:14:34 +0000

    ENH: Add pytest warns context manager manager

--- tests/test_series.py
+++ tests/test_series.py
@@ -649,7 +649,6 @@ def test_types_describe() -> None:
         s.describe(percentiles=[0.5], include="all")
     with pytest.warns(DeprecationWarning, match="elementwise comparison failed"):
         s.describe(exclude=np.number)
-    # datetime_is_numeric param added in 1.1.0 https://pandas.pydata.org/docs/whatsnew/v1.1.0.html
     if PD_LTE_15:
         # datetime_is_numeric param added in 1.1.0
         # https://pandas.pydata.org/docs/whatsnew/v1.1.0.html
