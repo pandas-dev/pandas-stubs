ae416773c83c380fd89962515d64d91115bd2839
Kevin Sheppard <kevin.sheppard@gmail.com>
2022-11-06 13:16:58 +0000

    ENH: Add missing typing found by coverage

--- pandas-stubs/_version.pyi
+++ pandas-stubs/_version.pyi
@@ -1,7 +1,21 @@
-from typing import Literal
+from typing import (
+    Literal,
+    TypedDict,
+)
+
+_VersionInfo = TypedDict(
+    "_VersionInfo",
+    {
+        "date": str,
+        "dirty": bool,
+        "error": str | None,
+        "full-revisionid": str,
+        "version": str,
+    },
+)
 
 version_json: str = ...
 
-def get_versions(): ...
+def get_versions() -> _VersionInfo: ...
 
 _stub_version: Literal["1.5.1.221024"]
