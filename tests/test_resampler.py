1cf02caddce3531fbee56bc7bda10cc577696798
Torsten Wörtwein <twoertwein@gmail.com>
2022-11-10 16:13:55 -0500

    fix tests

--- tests/test_resampler.py
+++ tests/test_resampler.py
@@ -16,12 +16,14 @@ from pandas import (
 )
 from pandas.core.groupby.generic import SeriesGroupBy
 from pandas.core.resample import Resampler
-import pytest
 from typing_extensions import assert_type
 
 from pandas._typing import Scalar
 
-from tests import check
+from tests import (
+    check,
+    pytest_warns_bounded,
+)
 
 DR = date_range("1999-1-1", periods=365, freq="D")
 DF_ = DataFrame(np.random.standard_normal((365, 1)), index=DR)
@@ -298,7 +300,10 @@ def test_aggregate_series_combinations() -> None:
 
     check(S.resample("m").aggregate(np.sum), Series)
     check(S.resample("m").aggregate("sum"), Series)
-    check(S.resample("m").aggregate(s2series), Series)
+    with pytest_warns_bounded(
+        FutureWarning, match="Not prepending group keys", upper="1.5.99"
+    ):
+        check(S.resample("m").aggregate(s2series), Series)
     check(S.resample("m").aggregate(s2scalar), Series)
     check(S.resample("m").aggregate([np.mean]), DataFrame)
     check(S.resample("m").aggregate(["sum", np.mean]), DataFrame)
@@ -318,7 +323,10 @@ def test_aggregate_frame_combinations() -> None:
 
     check(DF.resample("m").aggregate(np.sum), DataFrame)
     check(DF.resample("m").aggregate("sum"), DataFrame)
-    check(DF.resample("m").aggregate(df2frame), DataFrame)
+    with pytest_warns_bounded(
+        FutureWarning, match="Not prepending group keys", upper="1.5.99"
+    ):
+        check(DF.resample("m").aggregate(df2frame), DataFrame)
     check(DF.resample("m").aggregate(df2series), DataFrame)
     check(DF.resample("m").aggregate(df2scalar), DataFrame)
     check(DF.resample("m").aggregate([np.mean]), DataFrame)
