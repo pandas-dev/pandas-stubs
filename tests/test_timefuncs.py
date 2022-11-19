9fc4c76c0dcea81866fbe8062480589924ce3e23
ramvikrams <ramvikrams243@gmail.com>
2022-11-19 15:02:14 +0530

    Update test_timefuncs.py

--- tests/test_timefuncs.py
+++ tests/test_timefuncs.py
@@ -1043,7 +1043,7 @@ def timedelta64_and_arithmatic_operator() -> None:
     s3 = s2 - s1
     td = np.timedelta64(1, "M")
     check(assert_type((s1 - td), pd.TimestampSeries), np.timedelta64)
-    check(assert_type((s1 + td), pd.Series), pd.Timestamp)
+    check(assert_type((s1 + td), pd.Series), pd.TimestampSeries)
     check(assert_type((s1 * td), pd.Series), pd.Timestamp)
     check(assert_type((s1 / td), pd.Series), pd.Timestamp)
     check(
