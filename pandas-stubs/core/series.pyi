6915b9b84d2ecefafc300e34e2f785979e4e2d74
Irv Lustig <irv@princeton.com>
2023-01-03 09:36:08 -0500

    revert changes related to Series.__new__ and IntervalT

--- pandas-stubs/core/series.pyi
+++ pandas-stubs/core/series.pyi
@@ -71,7 +71,6 @@ from typing_extensions import (
 )
 import xarray as xr
 
-from pandas._libs.interval import Interval
 from pandas._libs.missing import NAType
 from pandas._libs.tslibs import BaseOffset
 from pandas._typing import (
@@ -96,6 +95,7 @@ from pandas._typing import (
     IgnoreRaise,
     IndexingInt,
     IntervalClosedType,
+    IntervalT,
     JoinHow,
     JsonSeriesOrient,
     Level,
@@ -217,43 +217,13 @@ class Series(IndexOpsMixin, NDFrame, Generic[S1]):
     @overload
     def __new__(
         cls,
-        data: IntervalIndex[Interval[int]],
+        data: IntervalIndex[IntervalT],
         index: Axes | None = ...,
         dtype=...,
         name: Hashable | None = ...,
         copy: bool = ...,
         fastpath: bool = ...,
-    ) -> Series[Interval[int]]: ...
-    @overload
-    def __new__(
-        cls,
-        data: IntervalIndex[Interval[float]],
-        index: Axes | None = ...,
-        dtype=...,
-        name: Hashable | None = ...,
-        copy: bool = ...,
-        fastpath: bool = ...,
-    ) -> Series[Interval[float]]: ...
-    @overload
-    def __new__(
-        cls,
-        data: IntervalIndex[Interval[Timestamp]],
-        index: Axes | None = ...,
-        dtype=...,
-        name: Hashable | None = ...,
-        copy: bool = ...,
-        fastpath: bool = ...,
-    ) -> Series[Interval[Timestamp]]: ...
-    @overload
-    def __new__(
-        cls,
-        data: IntervalIndex[Interval[Timedelta]],
-        index: Axes | None = ...,
-        dtype=...,
-        name: Hashable | None = ...,
-        copy: bool = ...,
-        fastpath: bool = ...,
-    ) -> Series[Interval[Timedelta]]: ...
+    ) -> Series[IntervalT]: ...
     @overload
     def __new__(
         cls,
