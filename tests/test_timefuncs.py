e4204ca4b2649897f9e0f84c4c58684c9a27806a
Irv Lustig <irv@princeton.com>
2023-01-06 20:47:16 -0500

    fix nightly build with Series.to_numpy() on a Timedelta series

--- tests/test_timefuncs.py
+++ tests/test_timefuncs.py
@@ -28,6 +28,7 @@ if TYPE_CHECKING:
 else:
     FulldatetimeDict = Any
 from tests import (
+    PD_LTE_15,
     TYPE_CHECKING_INVALID_USAGE,
     check,
     pytest_warns_bounded,
@@ -652,7 +653,12 @@ def test_types_to_numpy() -> None:
     td_s = pd.to_timedelta(pd.Series([10, 20]), "minutes")
     check(assert_type(td_s.to_numpy(), np.ndarray), np.ndarray)
     check(assert_type(td_s.to_numpy(dtype="int", copy=True), np.ndarray), np.ndarray)
-    check(assert_type(td_s.to_numpy(na_value=0), np.ndarray), np.ndarray)
+    if PD_LTE_15:
+        check(assert_type(td_s.to_numpy(na_value=0), np.ndarray), np.ndarray)
+    else:
+        check(
+            assert_type(td_s.to_numpy(na_value=pd.Timedelta(0)), np.ndarray), np.ndarray
+        )
 
 
 def test_to_timdelta_units() -> None:
