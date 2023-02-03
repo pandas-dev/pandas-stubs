354406c7cae439e1f8d52377a91407345aad72e4
Irv Lustig <irv@princeton.com>
2023-02-03 14:45:40 -0500

    Support pd.cut on a series of timestamps (#507)
    * Support pd.cut on a series of timestamps
    
    * remove Series[Timestamp]

--- pandas-stubs/core/reshape/tile.pyi
+++ pandas-stubs/core/reshape/tile.pyi
@@ -7,12 +7,17 @@ from typing import (
 import numpy as np
 from pandas import (
     Categorical,
+    CategoricalDtype,
+    DatetimeIndex,
     Float64Index,
     Index,
     Int64Index,
+    Interval,
     IntervalIndex,
     Series,
+    Timestamp,
 )
+from pandas.core.series import TimestampSeries
 
 from pandas._typing import (
     Label,
@@ -46,6 +51,36 @@ def cut(
     ordered: bool = ...,
 ) -> tuple[npt.NDArray[np.intp], IntervalIndex]: ...
 @overload
+def cut(  # type: ignore[misc]
+    x: TimestampSeries,
+    bins: int
+    | TimestampSeries
+    | DatetimeIndex
+    | Sequence[Timestamp]
+    | Sequence[np.datetime64],
+    right: bool = ...,
+    labels: Literal[False] | Sequence[Label] | None = ...,
+    *,
+    retbins: Literal[True],
+    precision: int = ...,
+    include_lowest: bool = ...,
+    duplicates: Literal["raise", "drop"] = ...,
+    ordered: bool = ...,
+) -> tuple[Series, DatetimeIndex]: ...
+@overload
+def cut(
+    x: TimestampSeries,
+    bins: IntervalIndex[Interval[Timestamp]],
+    right: bool = ...,
+    labels: Sequence[Label] | None = ...,
+    *,
+    retbins: Literal[True],
+    precision: int = ...,
+    include_lowest: bool = ...,
+    duplicates: Literal["raise", "drop"] = ...,
+    ordered: bool = ...,
+) -> tuple[Series, DatetimeIndex]: ...
+@overload
 def cut(
     x: Series,
     bins: int | Series | Int64Index | Float64Index | Sequence[int] | Sequence[float],
@@ -61,7 +96,7 @@ def cut(
 @overload
 def cut(
     x: Series,
-    bins: IntervalIndex,
+    bins: IntervalIndex[Interval[int]] | IntervalIndex[Interval[float]],
     right: bool = ...,
     labels: Sequence[Label] | None = ...,
     *,
@@ -116,6 +151,23 @@ def cut(
     duplicates: Literal["raise", "drop"] = ...,
     ordered: bool = ...,
 ) -> npt.NDArray[np.intp]: ...
+@overload
+def cut(
+    x: TimestampSeries,
+    bins: int
+    | TimestampSeries
+    | DatetimeIndex
+    | Sequence[Timestamp]
+    | Sequence[np.datetime64]
+    | IntervalIndex[Interval[Timestamp]],
+    right: bool = ...,
+    labels: Literal[False] | Sequence[Label] | None = ...,
+    retbins: Literal[False] = ...,
+    precision: int = ...,
+    include_lowest: bool = ...,
+    duplicates: Literal["raise", "drop"] = ...,
+    ordered: bool = ...,
+) -> Series[CategoricalDtype]: ...
 @overload
 def cut(
     x: Series,
