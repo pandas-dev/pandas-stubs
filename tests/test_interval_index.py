89c4a027daec11a93fb8dfcedf0921d5e716b600
Torsten Wörtwein <twoertwein@gmail.com>
2023-02-17 14:24:16 -0500

    include DataFrame test

--- tests/test_interval_index.py
+++ tests/test_interval_index.py
@@ -44,7 +44,9 @@ def test_subclass() -> None:
     def index(test: pd.Index) -> None:
         ...
 
-    index(pd.IntervalIndex.from_tuples([(0, 1), (1, 2)]))
+    interval_index = pd.IntervalIndex.from_tuples([(0, 1), (1, 2)])
+    index(interval_index)
+    pd.DataFrame({"a": [1, 2]}, interval_index)
 
 
 def test_is_overlapping() -> None:
