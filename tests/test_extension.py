fbc5f20d35c0d031fc30dfe2cd8e1dfb21c8610a
ramvikrams <ramvikrams243@gmail.com>
2023-03-28 23:42:14 +0530

    gh-583:added tolist() (#589)
    * added tolist()
    
    * Update test_pandas.py
    
    * added the example
    
    * Update test_pandas.py
    
    * Update test_pandas.py
    
    * added the test
    
    * changed return type in array func and corrected the test

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,5 +1,6 @@
 import decimal
 
+import pandas as pd
 from typing_extensions import assert_type
 
 from tests import check
@@ -14,3 +15,13 @@ def test_constructor() -> None:
 
     check(assert_type(arr, DecimalArray), DecimalArray, decimal.Decimal)
     check(assert_type(arr.dtype, DecimalDtype), DecimalDtype)
+
+
+def test_tolist() -> None:
+    data = {"State": "Texas", "Population": 2000000, "GDP": "2T"}
+    s = pd.Series(data)
+    data1 = [1, 2, 3]
+    s1 = pd.Series(data1)
+    check(assert_type(s.array.tolist(), list), list)
+    check(assert_type(s1.array.tolist(), list), list)
+    check(assert_type(pd.array([1, 2, 3]).tolist(), list), list)
