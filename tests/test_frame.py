b3144d9b998d7fcfe33a5f187a7258dfa1435733
ljmc <louis.cochen@protonmail.ch>
2023-07-03 17:13:37 +0100

    Move tests to test_types_groupby_agg

--- tests/test_frame.py
+++ tests/test_frame.py
@@ -880,10 +880,6 @@ def test_types_groupby() -> None:
     s1: pd.Series = df.set_index("col1")["col2"]
     s2: pd.Series = s1.groupby("col1").transform("sum")
 
-    # GH 736
-    check(assert_type(df1.groupby(by="col1").aggregate("size"), pd.Series), pd.Series)
-    check(assert_type(df1.groupby(by="col1").agg("size"), pd.Series), pd.Series)
-
 
 def test_types_groupby_methods() -> None:
     df = pd.DataFrame(data={"col1": [1, 1, 2], "col2": [3, 4, 5], "col3": [0, 1, 0]})
@@ -955,6 +951,9 @@ def test_types_groupby_agg() -> None:
 
     cols_mixed: list[str | int] = ["col1", 0]
     check(assert_type(df.groupby(by=cols_mixed).sum(), pd.DataFrame), pd.DataFrame)
+    # GH 736
+    check(assert_type(df.groupby(by="col1").aggregate("size"), pd.Series), pd.Series)
+    check(assert_type(df.groupby(by="col1").agg("size"), pd.Series), pd.Series)
 
 
 # This was added in 1.1.0 https://pandas.pydata.org/docs/whatsnew/v1.1.0.html
