c2bb19d8cfbfa2e375c39c960cc7315f43717965
Loic Diridollou <loic.diridollou@gmail.com>
2024-08-04 15:24:55 -0700

    GH 927 Allow 'integer' in select_dtypes include argument

--- pandas-stubs/core/frame.pyi
+++ pandas-stubs/core/frame.pyi
@@ -624,6 +624,7 @@ class DataFrame(NDFrame, OpsMixin):
             "number",
             "datetime64",
             "datetime",
+            "integer",
             "timedelta",
             "timedelta64",
             "datetimetz",
