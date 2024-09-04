9c58445ced9c799c4c020b8e22541bdfeb2da0d4
cmp0xff <cmp0xff@gmail.com>
2024-09-04 10:04:43 +0200

    feat: #983 new TypeVar, following https://github.com/pandas-dev/pandas-stubs/pull/990#issuecomment-2327478740

--- pandas-stubs/core/strings.pyi
+++ pandas-stubs/core/strings.pyi
@@ -12,6 +12,7 @@ from typing import (
 )
 
 import numpy as np
+from numpy import typing as npt
 import pandas as pd
 from pandas import (
     DataFrame,
@@ -27,8 +28,10 @@ from pandas._typing import (
 
 # The _TS type is what is used for the result of str.split with expand=True
 _TS = TypeVar("_TS", DataFrame, MultiIndex)
+# The _TM type is what is used for the result of str.match
+_TM = TypeVar("_TM", Series[bool], npt.NDArray[np.bool_])
 
-class StringMethods(NoNewAttributesMixin, Generic[T, _TS]):
+class StringMethods(NoNewAttributesMixin, Generic[T, _TS, _TM]):
     def __init__(self, data: T) -> None: ...
     def __getitem__(self, key: slice | int) -> T: ...
     def __iter__(self) -> T: ...
@@ -100,7 +103,7 @@ class StringMethods(NoNewAttributesMixin, Generic[T, _TS]):
     ) -> Series[bool]: ...
     def match(
         self, pat: str, case: bool = ..., flags: int = ..., na: Any = ...
-    ) -> Series[bool]: ...
+    ) -> _TM: ...
     def replace(
         self,
         pat: str,
