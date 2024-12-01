d4d399ebaa2e4b56a507a7272b80f14740e71499
Loic Diridollou <loic.diridollou@gmail.com>
2024-11-30 21:22:39 -0500

    GH1055 Add pandas.api.typing to pandas-stubs (#1058)
    * GH1055 Add pandas.api.typing to pandas-stubs
    
    * GH1055 Fix lint
    
    * GH1055 Fix assert_type
    
    * GH1055 Fix tests
    
    * Revert to generic
    
    * GH1055 Fix tests
    
    * GH1055 new test format
    
    * GH1055 PR Feedback
    
    * GH1055 TypeAlias Feedback

---
+++ pandas-stubs/api/typing/__init__.pyi
@@ -0,0 +1,29 @@
+from pandas.core.groupby import (
+    DataFrameGroupBy as DataFrameGroupBy,
+    SeriesGroupBy as SeriesGroupBy,
+)
+from pandas.core.resample import (
+    DatetimeIndexResamplerGroupby as DatetimeIndexResamplerGroupby,
+    PeriodIndexResamplerGroupby as PeriodIndexResamplerGroupby,
+    Resampler as Resampler,
+    TimedeltaIndexResamplerGroupby as TimedeltaIndexResamplerGroupby,
+    TimeGrouper as TimeGrouper,
+)
+from pandas.core.window import (
+    Expanding as Expanding,
+    ExpandingGroupby as ExpandingGroupby,
+    ExponentialMovingWindow as ExponentialMovingWindow,
+    ExponentialMovingWindowGroupby as ExponentialMovingWindowGroupby,
+    Rolling as Rolling,
+    RollingGroupby as RollingGroupby,
+    Window as Window,
+)
+
+from pandas._libs import NaTType as NaTType
+from pandas._libs.missing import NAType as NAType
+
+from pandas.io.json._json import JsonReader as JsonReader
+
+# SASReader is not defined so commenting it out for now
+# from pandas.io.sas.sasreader import SASReader as SASReader
+from pandas.io.stata import StataReader as StataReader
