b7a991b5c5476c2a8a35c70bc889024bfb4f9988
Loïc Diridollou <loic.diridollou@gmail.com>
2025-05-21 19:21:20 -0400

    GH1226 PR Feedback

--- tests/test_styler.py
+++ tests/test_styler.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 import os
 import pathlib
-from typing import (
-    TYPE_CHECKING,
-    Any,
-)
+from typing import TYPE_CHECKING
 
 from jinja2.environment import (
     Environment,
