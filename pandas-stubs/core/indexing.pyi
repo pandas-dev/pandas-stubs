d0ed65c3a07a9f4df1b0271c39f4b255ecb4554b
Danny Yang <yangdanny@fb.com>
2025-06-24 20:38:43 -0400

    add suppressions

--- pandas-stubs/core/indexing.pyi
+++ pandas-stubs/core/indexing.pyi
@@ -17,8 +17,8 @@ _IndexSliceTuple: TypeAlias = tuple[
 _IndexSliceUnion: TypeAlias = slice | _IndexSliceTuple
 
 _IndexSliceUnionT = TypeVar(
-    "_IndexSliceUnionT", bound=_IndexSliceUnion
-)  # pyrefly: ignore
+    "_IndexSliceUnionT", bound=_IndexSliceUnion  # pyrefly: ignore
+)
 
 class _IndexSlice:
     def __getitem__(self, arg: _IndexSliceUnionT) -> _IndexSliceUnionT: ...
