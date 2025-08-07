6155d44c2f449dec0d846b841ec30c3d00b55f3d
Loic Diridollou <loic.diridollou@gmail.com>
2025-08-07 10:58:16 -0400

    GH804 Fix property setter/getter for Index.names (#1308)
    * GH804 Fix property setter/getter for Index.names
    
    * GH804 PR feedback

--- tests/test_indexes.py
+++ tests/test_indexes.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections.abc import Hashable
 import datetime as dt
 from typing import (
     TYPE_CHECKING,
@@ -1433,3 +1434,23 @@ def test_multiindex_range() -> None:
         [range(3), pd.Series([2, 3, 5])],
     )
     check(assert_type(midx_mixed_types, pd.MultiIndex), pd.MultiIndex)
+
+
+def test_index_naming() -> None:
+    """
+    Test index names type both for the getter and the setter.
+    The names of an index should be settable with a sequence (not str) and names
+    property is a list[Hashable | None] (FrozenList).
+    """
+    df = pd.DataFrame({"a": ["a", "b", "c"], "i": [10, 11, 12]})
+
+    df.index.names = ["idx"]
+    check(assert_type(df.index.names, list[Hashable | None]), list)
+    df.index.names = [3]
+    check(assert_type(df.index.names, list[Hashable | None]), list)
+    df.index.names = ("idx2",)
+    check(assert_type(df.index.names, list[Hashable | None]), list)
+    df.index.names = [None]
+    check(assert_type(df.index.names, list[Hashable | None]), list)
+    df.index.names = (None,)
+    check(assert_type(df.index.names, list[Hashable | None]), list)
