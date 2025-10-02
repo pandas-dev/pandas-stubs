cdcb176391bdf350b7ce9b1d785ce70bf47355de
cmp0xff <cmp0xff@gmail.com>
2025-10-02 15:40:03 +0200

    use pd.array directly

--- tests/arrays/test_arrays.py
+++ tests/arrays/test_arrays.py
@@ -1,10 +1,5 @@
-from typing import cast
-
-from pandas.core.arrays.integer import (
-    Int64Dtype,
-    IntegerArray,
-)
-from pandas.core.series import Series
+from pandas.core.arrays.integer import IntegerArray
+from pandas.core.construction import array
 from typing_extensions import assert_type
 
 from pandas._libs.missing import NA
@@ -12,28 +7,6 @@ from pandas._libs.missing import NA
 from tests import check
 
 
-def test_cumul_int64dtype():
-    # cast will be removed if pandas-dev/pandas-stubs#1395 is resolved
-    arr = cast("Series[Int64Dtype]", Series([1, NA, 2], dtype="Int64")).array
-
-    check(assert_type(arr._accumulate("cummin"), IntegerArray), IntegerArray)
-    check(assert_type(arr._accumulate("cummax"), IntegerArray), IntegerArray)
-    check(assert_type(arr._accumulate("cumsum"), IntegerArray), IntegerArray)
-    check(assert_type(arr._accumulate("cumprod"), IntegerArray), IntegerArray)
-
-    check(
-        assert_type(arr._accumulate("cummin", skipna=False), IntegerArray),
-        IntegerArray,
-    )
-    check(
-        assert_type(arr._accumulate("cummax", skipna=False), IntegerArray),
-        IntegerArray,
-    )
-    check(
-        assert_type(arr._accumulate("cumsum", skipna=False), IntegerArray),
-        IntegerArray,
-    )
-    check(
-        assert_type(arr._accumulate("cumprod", skipna=False), IntegerArray),
-        IntegerArray,
-    )
+def test_construction() -> None:
+    check(assert_type(array([1]), IntegerArray), IntegerArray)
+    check(assert_type(array([1, NA]), IntegerArray), IntegerArray)
