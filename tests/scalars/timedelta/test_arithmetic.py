43e895488fedfddc36eb238c88acfe65d02b31ab
Loïc Diridollou <loic.diridollou@gmail.com>
2025-10-10 17:10:18 -0400

    GH1418 PR Feedback

--- tests/scalars/timedelta/test_arithmetic.py
+++ tests/scalars/timedelta/test_arithmetic.py
@@ -15,11 +15,16 @@ def test_mul() -> None:
     b = True
     c = 1.0
     d = 5
+    e = 1 + 3.0j
 
     check(assert_type(a * c, pd.Timedelta), pd.Timedelta)
+    check(assert_type(c * a, pd.Timedelta), pd.Timedelta)
     check(assert_type(a * d, pd.Timedelta), pd.Timedelta)
+    check(assert_type(d * a, pd.Timedelta), pd.Timedelta)
 
     if TYPE_CHECKING_INVALID_USAGE:
         # pd.Timedelta * bool is not allowed, see GH1418
         _0 = a * b  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
         _1 = b * a  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _2 = a * e  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _3 = e * a  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
