8ec49016cdb89b0e42d26c76e2eb964aaca61e70
Yi-Fan Wang <cmp0xff@users.noreply.github.com>
2025-10-11 21:45:09 +0200

    fix(arithmetic): #1418 🈲 disallow bool * timedelta for 3.0 (#1422)
    * fix: #1418 disallow bool * timedelta
    
    * attempt to fix mypy for 3.10

--- pandas-stubs/_typing.pyi
+++ pandas-stubs/_typing.pyi
@@ -872,9 +872,6 @@ SeriesDType: TypeAlias = (
     | datetime.timedelta  # includes pd.Timedelta
 )
 S1 = TypeVar("S1", bound=SeriesDType, default=Any)
-S1_CO_NSDT = TypeVar(
-    "S1_CO_NSDT", bound=SeriesDTypeNoStrDateTime, default=Any, covariant=True
-)
 S1_CT_NDT = TypeVar(
     "S1_CT_NDT", bound=SeriesDTypeNoDateTime, default=Any, contravariant=True
 )
@@ -882,6 +879,8 @@ S1_CO = TypeVar("S1_CO", bound=SeriesDType, default=Any, covariant=True)
 S1_CT = TypeVar("S1_CT", bound=SeriesDType, default=Any, contravariant=True)
 # Like S1, but without `default=Any`.
 S2 = TypeVar("S2", bound=SeriesDType)
+S2_CT = TypeVar("S2_CT", bound=SeriesDType, contravariant=True)
+S2_CO_NSDT = TypeVar("S2_CO_NSDT", bound=SeriesDTypeNoStrDateTime, covariant=True)
 S3 = TypeVar("S3", bound=SeriesDType)
 
 # Constraint, instead of bound
--- tests/indexes/arithmetic/timedelta/test_mul.py
+++ tests/indexes/arithmetic/timedelta/test_mul.py
@@ -10,7 +10,6 @@ from typing_extensions import (
 )
 
 from tests import (
-    PD_LTE_23,
     TYPE_CHECKING_INVALID_USAGE,
     check,
 )
@@ -29,40 +28,38 @@ def test_mul_py_scalar(left: "pd.Index[pd.Timedelta]") -> None:
     """Test pd.Index[pd.Timedelta] * Python native scalars"""
     b, i, f, c = True, 1, 1.0, 1j
 
-    # pandas-dev/pandas#62316
-    if PD_LTE_23:
-        check(assert_type(left * b, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _00 = left * b  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(left * i, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(left * f, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _0 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _03 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
 
-    if PD_LTE_23:
-        check(assert_type(b * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _10 = b * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(i * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(f * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _1 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _13 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
 
 
 def test_mul_py_sequence(left: "pd.Index[pd.Timedelta]") -> None:
     """Test pd.Index[pd.Timedelta] * Python native sequences"""
     b, i, f, c = [True], [2], [1.5], [1.7j]
 
-    # pandas-dev/pandas#62316
-    if PD_LTE_23:
-        check(assert_type(left * b, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _00 = left * b  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(left * i, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(left * f, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _0 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _03 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
 
-    if PD_LTE_23:
-        check(assert_type(b * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _10 = b * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(i * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(f * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _1 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _13 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
 
 
 def test_mul_numpy_array(left: "pd.Index[pd.Timedelta]") -> None:
@@ -72,9 +69,8 @@ def test_mul_numpy_array(left: "pd.Index[pd.Timedelta]") -> None:
     f = np.array([1.5], np.float64)
     c = np.array([1.7j], np.complex128)
 
-    # pandas-dev/pandas#62316
-    if PD_LTE_23:
-        check(assert_type(left * b, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        assert_type(left * b, Never)
     check(assert_type(left * i, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(left * f, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
@@ -83,8 +79,8 @@ def test_mul_numpy_array(left: "pd.Index[pd.Timedelta]") -> None:
     # `numpy` typing gives the corresponding `ndarray`s in the static type
     # checking, where our `__rmul__` cannot override. At runtime, they return
     # `Series` with the correct element type.
-    if PD_LTE_23:
-        check(assert_type(b * left, "npt.NDArray[np.bool_]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        assert_type(b * left, "npt.NDArray[np.bool_]")
     check(assert_type(i * left, "npt.NDArray[np.int64]"), pd.Index, timedelta)
     check(assert_type(f * left, "npt.NDArray[np.float64]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
@@ -99,17 +95,16 @@ def test_mul_pd_index(left: "pd.Index[pd.Timedelta]") -> None:
     f = pd.Index([1.5])
     c = pd.Index([1.7j])
 
-    # pandas-dev/pandas#62316
-    if PD_LTE_23:
-        check(assert_type(left * b, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _00 = left * b  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(left * i, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(left * f, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _0 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _03 = left * c  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
 
-    if PD_LTE_23:
-        check(assert_type(b * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
+    if TYPE_CHECKING_INVALID_USAGE:
+        _10 = b * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
     check(assert_type(i * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     check(assert_type(f * left, "pd.Index[pd.Timedelta]"), pd.Index, timedelta)
     if TYPE_CHECKING_INVALID_USAGE:
-        _1 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
+        _13 = c * left  # type: ignore[operator] # pyright: ignore[reportOperatorIssue]
