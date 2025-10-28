56ba992b9d53522b6dbb9e49e64109f5db622369
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-10-28 11:46:28 +0000

    add tests

--- tests/indexes/test_rangeindex.py
+++ tests/indexes/test_rangeindex.py
@@ -16,3 +16,31 @@ def test_rangeindex_floordiv() -> None:
         assert_type(ri // 2, "pd.Index[int]"),
         pd.Index,
     )
+
+
+def test_rangeindex_min_max() -> None:
+    ri = pd.RangeIndex(3)
+    check(
+        assert_type(ri.min(), int),
+        int,
+    )
+    check(
+        assert_type(ri.max(axis=0), int),
+        int,
+    )
+
+
+def test_rangeindex_equals() -> None:
+    ri = pd.RangeIndex(3)
+    check(
+        assert_type(ri.equals(ri), bool),
+        bool,
+    )
+
+
+def test_rangeindex_tolist() -> None:
+    ri = pd.RangeIndex(3)
+    check(
+        assert_type(ri.tolist(), list[int]),
+        list[int],
+    )
