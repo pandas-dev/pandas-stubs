c5b0d15370dab9e97b9a35e80167f1d8cf8d41bc
yusufcank34 <yusufcankilic369@gmail.com>
2025-10-30 21:49:38 +0300

    Remove redundant test_datetime_index_1440.py and run pre-commit

--- tests/indexes/test_datetime_index_1440.py
+++
@@ -1,8 +0,0 @@
-from typing import assert_type
-import pandas as pd
-
-# GH#1440 – constructing Index from DatetimeIndex should return DatetimeIndex
-data = pd.date_range("2022-01-01", "2022-01-03", freq="D")
-idx = pd.Index(data, name="date")
-
-assert_type(idx, pd.DatetimeIndex)
