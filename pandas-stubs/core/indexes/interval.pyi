1f5d9d770f31be3b24e790d31d1f775e4c098e6d
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-10-30 14:56:21 +0000

    type `IntervalIndex` (#1458)
    * type `IntervalIndex`
    
    * update pyproject.toml

--- pandas-stubs/core/indexes/interval.pyi
+++ pandas-stubs/core/indexes/interval.pyi
@@ -6,7 +6,6 @@ import datetime as dt
 from typing import (
     Literal,
     TypeAlias,
-    final,
     overload,
 )
 
@@ -22,7 +21,6 @@ from pandas._libs.interval import (
 from pandas._typing import (
     DatetimeLike,
     DtypeArg,
-    FillnaOptions,
     Frequency,
     IntervalClosedType,
     IntervalT,
@@ -219,14 +217,6 @@ class IntervalIndex(ExtensionIndex[IntervalT, np.object_], IntervalMixin):
     @property
     def is_overlapping(self) -> bool: ...
     def get_loc(self, key: Label) -> int | slice | np_1darray[np.bool]: ...
-    @final
-    def get_indexer(
-        self,
-        target: Index,
-        method: FillnaOptions | Literal["nearest"] | None = ...,
-        limit: int | None = ...,
-        tolerance=...,
-    ) -> np_1darray[np.intp]: ...
     def get_indexer_non_unique(
         self, target: Index
     ) -> tuple[npt.NDArray[np.intp], npt.NDArray[np.intp]]: ...
