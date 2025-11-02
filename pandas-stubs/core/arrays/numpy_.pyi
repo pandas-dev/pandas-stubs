04fa895348d8d9a87dd98e23ee684d1ece640234
cmp0xff <cmp0xff@gmail.com>
2025-11-02 15:30:36 +0100

    add NumpyExtensionArray

--- pandas-stubs/core/arrays/numpy_.pyi
+++ pandas-stubs/core/arrays/numpy_.pyi
@@ -1,9 +1,12 @@
 import numpy as np
 from numpy.lib.mixins import NDArrayOperatorsMixin
+from pandas.core.arraylike import OpsMixin
+from pandas.core.arrays._mixins import NDArrayBackedExtensionArray
 from pandas.core.arrays.base import (
     ExtensionArray,
     ExtensionOpsMixin,
 )
+from pandas.core.strings.object_array import ObjectStringArrayMixin
 
 from pandas.core.dtypes.dtypes import ExtensionDtype
 
@@ -14,3 +17,6 @@ class PandasDtype(ExtensionDtype):
     def itemsize(self) -> int: ...
 
 class PandasArray(ExtensionArray, ExtensionOpsMixin, NDArrayOperatorsMixin): ...
+class NumpyExtensionArray(
+    OpsMixin, NDArrayBackedExtensionArray, ObjectStringArrayMixin
+): ...
