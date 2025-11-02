83675dbf57c264ad49f5920574fb6ce154317e50
cmp0xff <cmp0xff@gmail.com>
2025-11-02 15:30:36 +0100

    feat: add ExtensionArrays

---
+++ tests/arrays/test_categorical.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from pandas.core.arrays.categorical import Categorical
+from typing_extensions import assert_type
+
+from tests import check
+
+
+def test_constructor() -> None:
+    check(assert_type(pd.array(["🐼"], dtype="category"), Categorical), Categorical)
+    check(
+        assert_type(pd.array(np.array(["🐼"]), dtype="category"), Categorical),
+        Categorical,
+    )
+    check(
+        assert_type(pd.array(pd.array(["🐼"]), dtype="category"), Categorical),
+        Categorical,
+    )
+    check(
+        assert_type(pd.array(pd.Index(["🐼"]), dtype="category"), Categorical),
+        Categorical,
+    )
+    check(
+        assert_type(pd.array(pd.Series(["🐼"]), dtype="category"), Categorical),
+        Categorical,
+    )
+
+    check(
+        assert_type(pd.array(pd.array(["🐼"], dtype="category")), Categorical),
+        Categorical,
+    )
+    check(
+        assert_type(pd.array(pd.Index(["🐼"], dtype="category")), Categorical),
+        Categorical,
+    )
+    # TODO: Categorical Series pandas-dev/pandas-stubs#1415
+    # check(
+    #     assert_type(pd.array(pd.Series(["🐼"], dtype="category")), Categorical),
+    #     Categorical,
+    # )
