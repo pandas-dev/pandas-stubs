5372b47a3a8ff154847a65f1589c6b51a25a2270
cmp0xff <cmp0xff@gmail.com>
2025-11-04 12:45:15 +0100

    removed disappeared classes

--- pandas-stubs/core/arrays/numpy_.pyi
+++ pandas-stubs/core/arrays/numpy_.pyi
@@ -1,22 +1,7 @@
-import numpy as np
-from numpy.lib.mixins import NDArrayOperatorsMixin
 from pandas.core.arraylike import OpsMixin
 from pandas.core.arrays._mixins import NDArrayBackedExtensionArray
-from pandas.core.arrays.base import (
-    ExtensionArray,
-    ExtensionOpsMixin,
-)
 from pandas.core.strings.object_array import ObjectStringArrayMixin
 
-from pandas.core.dtypes.dtypes import ExtensionDtype
-
-class PandasDtype(ExtensionDtype):
-    @property
-    def numpy_dtype(self) -> np.dtype: ...
-    @property
-    def itemsize(self) -> int: ...
-
-class PandasArray(ExtensionArray, ExtensionOpsMixin, NDArrayOperatorsMixin): ...
 class NumpyExtensionArray(
     OpsMixin, NDArrayBackedExtensionArray, ObjectStringArrayMixin
 ): ...
