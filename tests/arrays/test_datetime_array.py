d7056b3fd9a37421805474a48333283a64b7a4ed
cmp0xff <cmp0xff@gmail.com>
2025-11-10 18:13:00 +0100

    mypy

--- tests/arrays/test_datetime_array.py
+++ tests/arrays/test_datetime_array.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from typing import cast
 
 import numpy as np
 import pandas as pd
 from pandas.core.arrays.datetimes import DatetimeArray
 from typing_extensions import assert_type
+
+from pandas._libs.tslibs.nattype import NaTType
 
 from tests import check
 
@@ -18,7 +21,8 @@ def test_constructor() -> None:
     np_dt = np.datetime64(dt)
     check(assert_type(pd.array([np_dt]), DatetimeArray), DatetimeArray)
     check(assert_type(pd.array([np_dt, None]), DatetimeArray), DatetimeArray)
-    check(assert_type(pd.array([np_dt, pd.NaT]), DatetimeArray), DatetimeArray)
+    dt_nat = cast(list[np.datetime64 | NaTType], [np_dt, pd.NaT])
+    check(assert_type(pd.array(dt_nat), DatetimeArray), DatetimeArray)
 
     check(
         assert_type(  # type: ignore[assert-type]  # I do not understand
