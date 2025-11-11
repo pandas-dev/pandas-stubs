be5fc306fbe057b9ab724f4cb61ba2330d6dc461
cmp0xff <cmp0xff@gmail.com>
2025-11-11 22:42:42 +0100

    interval array

---
+++ tests/arrays/test_interval_array.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pandas.core.arrays.interval import IntervalArray
+from typing_extensions import assert_type
+
+from tests import check
+
+
+def test_constructor() -> None:
+    itv = pd.Interval(0, 1)
+    check(
+        assert_type(  # type: ignore[assert-type]  # I do not understand
+            pd.array([itv]), IntervalArray
+        ),
+        IntervalArray,
+    )
+    check(
+        assert_type(  # type: ignore[assert-type]  # I do not understand
+            pd.array([itv, None]), IntervalArray
+        ),
+        IntervalArray,
+    )
+
+    check(
+        assert_type(  # type: ignore[assert-type]  # I do not understand
+            pd.array(pd.array([itv])), IntervalArray
+        ),
+        IntervalArray,
+    )
+
+    check(assert_type(pd.array(pd.Index([itv])), IntervalArray), IntervalArray)
+
+    check(assert_type(pd.array(pd.Series([itv])), IntervalArray), IntervalArray)
