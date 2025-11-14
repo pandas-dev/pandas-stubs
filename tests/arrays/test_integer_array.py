bef16f09edf936198fd9a1a0a96bc5d44d3395b6
cmp0xff <cmp0xff@gmail.com>
2025-11-14 17:14:13 +0100

    dtype numpy extension array

--- tests/arrays/test_integer_array.py
+++ tests/arrays/test_integer_array.py
@@ -12,11 +12,7 @@ def test_constructor() -> None:
     check(assert_type(pd.array([1, None]), IntegerArray), IntegerArray)
     check(assert_type(pd.array([1, pd.NA, None]), IntegerArray), IntegerArray)
 
-    check(
-        assert_type(  # type: ignore[assert-type] # I do not understand
-            pd.array(np.array([1], np.int64)), IntegerArray
-        ),
-        IntegerArray,
-    )
+    np_arr = np.array([1], np.int64)
+    check(assert_type(pd.array(np_arr), IntegerArray), IntegerArray)
 
     check(assert_type(pd.array(pd.array([1])), IntegerArray), IntegerArray)
--- tests/arrays/test_numpy_extension_array.py
+++ tests/arrays/test_numpy_extension_array.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
 from pandas.core.arrays.numpy_ import NumpyExtensionArray
+import pytest
 from typing_extensions import assert_type
 
-from tests import check
+from tests import (
+    check,
+    get_dtype,
+)
+from tests._typing import (
+    BuiltinDtypeArg,
+    NumpyNotTimeDtypeArg,
+)
 
 
 def test_constructor() -> None:
@@ -11,18 +19,16 @@ def test_constructor() -> None:
     #     assert_type(pd.array([pd.NA, None]), NumpyExtensionArray), NumpyExtensionArray
     # )
 
+    pd_arr = pd.array([1, "🐼"])
     check(
         assert_type(  # type: ignore[assert-type] # I do not understand
-            pd.array([1, "🐼"]), NumpyExtensionArray
+            pd_arr, NumpyExtensionArray
         ),
         NumpyExtensionArray,
     )
-    check(
-        assert_type(  # type: ignore[assert-type] # I do not understand
-            pd.array(np.array([1, "🐼"], np.object_)), NumpyExtensionArray
-        ),
-        NumpyExtensionArray,
-    )
+
+    np_arr = np.array([1, "🐼"], np.object_)
+    check(assert_type(pd.array(np_arr), NumpyExtensionArray), NumpyExtensionArray)
     # check(
     #     assert_type(pd.array(pd.array([pd.NA, None])), NumpyExtensionArray),
     #     NumpyExtensionArray,
@@ -31,3 +37,17 @@ def test_constructor() -> None:
         assert_type(pd.array(pd.RangeIndex(0, 1)), NumpyExtensionArray),
         NumpyExtensionArray,
     )
+
+
+@pytest.mark.parametrize("dtype", get_dtype(BuiltinDtypeArg | NumpyNotTimeDtypeArg))
+def test_constructors_dtype(dtype: BuiltinDtypeArg | NumpyNotTimeDtypeArg):
+    if dtype == "V" or "void" in str(dtype):
+        check(
+            assert_type(pd.array([b"1"], dtype=dtype), NumpyExtensionArray),
+            NumpyExtensionArray,
+        )
+    else:
+        check(
+            assert_type(pd.array([1], dtype=dtype), NumpyExtensionArray),
+            NumpyExtensionArray,
+        )
