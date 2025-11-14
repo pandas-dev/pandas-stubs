2d0901431ce1f07f6fc9ec7a64df7655d8fce358
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-11-14 10:18:12 +0000

    add note about `from_dataframe` input type

--- pandas-stubs/core/interchange/from_dataframe.pyi
+++ pandas-stubs/core/interchange/from_dataframe.pyi
@@ -2,4 +2,5 @@ from typing import Any
 
 import pandas as pd
 
+# TODO: resolve with https://github.com/pandas-dev/pandas/issues/63111
 def from_dataframe(df: Any, allow_copy: bool = True) -> pd.DataFrame: ...
