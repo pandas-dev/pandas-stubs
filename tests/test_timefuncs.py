2b2d4df90ae82ec103182c1d06e08cf2e61a5170
Loïc Diridollou <loic.diridollou@gmail.com>
2025-11-27 09:01:10 -0500

    GH1432 PR Feedback

--- tests/test_timefuncs.py
+++ tests/test_timefuncs.py
@@ -94,15 +94,20 @@ def test_types_arithmetic() -> None:
     ts = pd.to_datetime("2021-03-01")
     ts2 = pd.to_datetime("2021-01-01")
     ts_np = np.datetime64("2021-01-01")
+    ts_np_time = np.datetime64("2021-01-01 08:00:05")
     delta = pd.to_timedelta("1 day")
 
     check(assert_type(ts - ts2, pd.Timedelta), pd.Timedelta)
     check(assert_type(ts - ts_np, pd.Timedelta), pd.Timedelta)
-    # TODO: pandas-dev/pandas-stubs#1432 mypy sees datetime.timedelta but pyright is correct
-    # check(assert_type(ts_np - ts, pd.Timedelta), pd.Timedelta)
+    check(assert_type(ts - ts_np_time, pd.Timedelta), pd.Timedelta)
     check(assert_type(ts + delta, pd.Timestamp), pd.Timestamp)
     check(assert_type(ts - delta, pd.Timestamp), pd.Timestamp)
     check(assert_type(ts - dt.datetime(2021, 1, 3), pd.Timedelta), pd.Timedelta)
+
+    if TYPE_CHECKING_INVALID_USAGE:
+        # TODO: pandas-dev/pandas-stubs#1432 mypy sees datetime.timedelta but pyright is correct
+        assert_type(ts_np - ts, pd.Timedelta)  # type: ignore[assert-type]
+        assert_type(ts_np_time - ts, pd.Timedelta)  # type: ignore[assert-type]
 
 
 def test_types_comparison() -> None:
