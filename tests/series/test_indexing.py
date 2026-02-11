2ab984c620f27eef7e54a3bb6ecbde714482ddfe
傅立业（Chris Fu） <17433201@qq.com>
2026-02-11 10:32:51 +0800

    Fix newly added part in `test_series_loc_setitem` so that it could be runnable

--- tests/series/test_indexing.py
+++ tests/series/test_indexing.py
@@ -94,8 +94,8 @@ def test_series_loc_setitem() -> None:
     s.loc[[0, 2, 4]] = v
 
     # GH 1669
-    s = pd.Series[int]([1, 2, 3, 4, 5], index=["a", "b", "c", "d", "e"])
-    idx = pd.Index[str](["a", "c", "e"])
+    s = pd.Series([1, 2, 3, 4, 5], index=["a", "b", "c", "d", "e"])
+    idx = pd.Index(["a", "c", "e"])
     v = s.loc[idx].values
     s.loc[idx] = v
 
