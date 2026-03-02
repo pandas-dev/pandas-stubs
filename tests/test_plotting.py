12cd204c3729bf4bc0d9973ac99d5c191cdd0647
Loïc Diridollou <loic.diridollou@gmail.com>
2026-03-02 11:22:18 -0500

    GH1654 Plotting changes in 3.0

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -765,3 +765,44 @@ def test_grouped_series_hist(close_figures: None) -> None:
         ),
         Series,
     )
+
+
+def test_plot_pie_no_ylabel(close_figures: None) -> None:
+    """Test passing no ylabel to the pie chart (new in 3.0)."""
+    sr = pd.Series([1, 2, 4], index=["a", "b", "c"], name="my_series")
+    sr.plot(kind="pie", xlabel=None)
+    sr.plot(kind="pie", ylabel=None)
+
+    df = sr.to_frame()
+    df.plot(kind="pie", xlabel=None, y="my_series")
+    df.plot(kind="pie", ylabel=None, y="my_series")
+
+
+def test_plot_kde_weights(close_figures: None) -> None:
+    """Test plotting a series with kde and weights."""
+    s = pd.Series([1, 2, 2.5, 3, 3.5, 4, 5])
+
+    # with numpy array
+    np_wgts = np.array([0.1, 0.0, 0.0, 0.2, 0.3, 0.4, 0.9])
+    s.plot.kde(weights=np_wgts)
+
+    # with series
+    sr_wgts = pd.Series([0.1, 0.0, 0.0, 0.2, 0.3, 0.4, 0.9])
+    s.plot.kde(weights=sr_wgts)
+
+
+def test_plot_df_scatter_color(close_figures: None) -> None:
+    """Test passing columns of strings colors parameter c for scatter plot (new in 3.0)."""
+    colors = ["NY", "MD", "MA", "CA"]
+
+    # When
+    df = pd.DataFrame(
+        {
+            "dataX": range(100),
+            "dataY": range(100),
+            "color": (colors[i % len(colors)] for i in range(100)),
+        }
+    )
+
+    # Then
+    df.plot.scatter("dataX", "dataY", c="color")
