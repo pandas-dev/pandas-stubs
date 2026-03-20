3c712deea9fd19756ef1b2fac7e61b12e95cd13c
Sidra-009 <sidrasaqlain11@gmail.com>
2026-03-20 14:03:35 +0500

    fix: clean up formatting and revert unrelated changes

--- pandas-stubs/tseries/frequencies.pyi
+++ pandas-stubs/tseries/frequencies.pyi
@@ -1,17 +1,15 @@
+from datetime import timedelta
 from typing import overload
-
 from pandas import (
     DatetimeIndex,
     Series,
     TimedeltaIndex,
 )
-
 from pandas._typing import Frequency
-
 from pandas.tseries.offsets import BaseOffset
 
 @overload
 def to_offset(freq: None, is_period: bool = False) -> None: ...
 @overload
-def to_offset(freq: Frequency, is_period: bool = False) -> BaseOffset: ...
+def to_offset(freq: Frequency | timedelta, is_period: bool = False) -> BaseOffset: ...
 def infer_freq(index: Series | DatetimeIndex | TimedeltaIndex) -> str | None: ...
