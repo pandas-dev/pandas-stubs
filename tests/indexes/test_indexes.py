ca916c2d7b1ee79ee04cc5dc4be629568463e900
Tine Zivic <tine.zivic@gmail.com>
2026-04-25 17:12:09 +0200

    Fix ignore comment for droplevel() — call-arg/reportCallIssue

--- tests/indexes/test_indexes.py
+++ tests/indexes/test_indexes.py
@@ -1618,7 +1618,7 @@ def test_index_droplevel() -> None:
     check(assert_type(mi.droplevel(("elk",)), pd.MultiIndex | pd.Index), pd.Index)
     check(assert_type(mi.droplevel(0), pd.MultiIndex | pd.Index), pd.Index)
     if TYPE_CHECKING_INVALID_USAGE:
-        idx.droplevel()  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
+        idx.droplevel()  # type: ignore[call-arg] # pyright: ignore[reportCallIssue]
         idx.droplevel(0)  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
         idx.droplevel([0])  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
         idx.droplevel("name")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
