406b31e5f0574c9b30c5939785147b38b3906815
Loic Diridollou <loic.diridollou@gmail.com>
2026-05-14 16:46:27 -0400

    MNT: Update to pandas 3.0.3 (#1744)

--- tests/series/test_agg.py
+++ tests/series/test_agg.py
@@ -60,14 +60,14 @@ def test_agg_complex() -> None:
     with pytest_warns_bounded(
         np.exceptions.ComplexWarning,
         r"Casting complex values to real discards the imaginary part",
-        upper="3.0.99",
+        upper="3.0.3",
     ):
         check(assert_type(series.std(), np.float64), np.float64)
 
     with pytest_warns_bounded(
         np.exceptions.ComplexWarning,
         r"Casting complex values to real discards the imaginary part",
-        upper="3.0.99",
+        upper="3.0.3",
     ):
         check(assert_type(series.var(), float), np.float64)
 
