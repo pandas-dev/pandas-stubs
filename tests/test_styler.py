d8271ed6bbe0c62c0be20121c59ddf36ba94dbf2
Reid Sox-Harris <reid@soxharris.com>
2026-05-22 23:11:57 -0400

    fixup! BUG: allow kwargs in Styler apply_index and map_index

--- tests/test_styler.py
+++ tests/test_styler.py
@@ -77,11 +77,14 @@ def test_apply_index() -> None:
     check(assert_type(DF.style.apply_index(f1), Styler), Styler)
 
     # GH 1723
-    def highlight_odd(index: pd.Series, /, color: str) -> list[str]:
+    def highlight_odd(index: pd.Series, color: str) -> list[str]:
         return [f"color: {color}" if x % 2 else "" for x in index]
 
     check(
-        assert_type(DF.style.apply_index(highlight_odd, color="purple"), Styler), Styler
+        assert_type(
+            DF.style.apply_index(highlight_odd, axis=0, color="purple"), Styler
+        ),
+        Styler,
     )
 
 
@@ -91,10 +94,10 @@ def test_map_index() -> None:
 
     check(assert_type(DF.style.map_index(f), Styler), Styler)
 
-    def f1(s: Scalar, /, color: str) -> str | None:
+    def f1(s: Scalar, color: str) -> str | None:
         return f"background-color: {color};" if s == "b" else None
 
-    check(assert_type(DF.style.map_index(f1, color="pink"), Styler), Styler)
+    check(assert_type(DF.style.map_index(f1, color="pink", axis=0), Styler), Styler)
 
 
 def test_background_gradient() -> None:
