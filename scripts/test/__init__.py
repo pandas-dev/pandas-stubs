6c0c5fbb5a691e21f3f577afaa5bdf0273d26096
cmp0xff <cmp0xff@gmail.com>
2026-06-29 17:56:38 +0200

    chore: add todo

--- scripts/test/__init__.py
+++ scripts/test/__init__.py
@@ -24,7 +24,7 @@ _DIST_STEPS = [
     _step.install_dist,
     _step.rename_src,
     _step.ty_dist,
-    # _step.pyrefly_dist,
+    # _step.pyrefly_dist,  TODO: pandas-dev/pandas-stubs#1801
     _step.pyright_dist,
     _step.mypy_dist,
 ]
