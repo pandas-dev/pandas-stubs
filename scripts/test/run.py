bc7da87854e962bbdbd32f464806a5ab8ffe3ece
cmp0xff <cmp0xff@gmail.com>
2026-06-29 17:16:15 +0200

    chore: enable ty on tests

--- scripts/test/run.py
+++ scripts/test/run.py
@@ -70,6 +70,16 @@ def pyright_dist() -> None:
     subprocess.run(cmd, check=True)
 
 
+def pyrefly_dist() -> None:
+    cmd = ["pyrefly", "check", "tests"]
+    subprocess.run(cmd, check=True)
+
+
+def ty_dist() -> None:
+    cmd = ["ty", "check", "tests"]
+    subprocess.run(cmd, check=True)
+
+
 def uninstall_dist() -> None:
     cmd = [sys.executable, "-m", "pip", "uninstall", "-y", "pandas-stubs"]
     subprocess.run(cmd, check=True)
@@ -148,13 +158,7 @@ def released_mypy() -> None:
 
 
 def ty() -> None:
-    cmd = [
-        "ty",
-        "check",
-        "pandas-stubs",
-        "--python",
-        sys.executable,
-    ]
+    cmd = ["ty", "check", "pandas-stubs", "tests", "--python", sys.executable]
     subprocess.run(cmd, check=True)
 
 
