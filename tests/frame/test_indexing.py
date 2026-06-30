7b28ccb88c40bfea6dfbc46e5be4e41877daa14c
Geoffrey Eisenbarth <geoffrey.eisenbarth@gmail.com>
2026-06-30 11:10:15 -0500

    Add tests

--- tests/frame/test_indexing.py
+++ tests/frame/test_indexing.py
@@ -574,9 +574,13 @@ def test_loc_list_str() -> None:
 
 
 def test_loc_returns_series() -> None:
-    df1 = pd.DataFrame({"x": [1, 2, 3, 4]}, index=[10, 20, 30, 40])
-    df2 = df1.loc[10, :]
-    check(assert_type(df2, pd.Series | pd.DataFrame), pd.Series)
+    df = pd.DataFrame(
+      {"x": [1, 2, 3, 4], "y": [10, 20, 30, 40]},
+      index=[10, 20, 30, 40]
+    )
+    check(assert_type(df.loc[10, :], pd.Series | pd.DataFrame), pd.Series)
+    check(assert_type(df.loc[10, ["x", "y"]], pd.Series | pd.DataFrame), pd.Series)
+    check(assert_type(df.loc[[10, 20], "x"], pd.Series), pd.Series)
 
 
 def test_frame_single_slice() -> None:
