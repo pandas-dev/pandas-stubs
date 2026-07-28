a411d50b66dcb425f0f51b87fd14aaa41de8f75b
cmp0xff <cmp0xff@gmail.com>
2026-07-28 09:16:35 +0200

    astral-sh/ty#3199, astral-sh/ty#4102

--- tests/arrays/test_base_string_.py
+++ tests/arrays/test_base_string_.py
@@ -1,3 +1,5 @@
+# ty: ignore[no-matching-overload,type-assertion-failure]
+# TODO: remove the above ignores astral-sh/ty#3199, astral-sh/ty#4102
 from collections import UserList
 from collections.abc import (
     Callable,
