cf88114c268215fd20581521a9f38dc129dc0901
Yi-Fan Wang <cmp0xff@gmail.com>
2026-08-02 13:00:59 +0200

    black

--- tests/frame/test_frame.py
+++ tests/frame/test_frame.py
@@ -89,7 +89,9 @@ def rands_array(nchars: int, size: int) -> np_ndarray:
     chars = np.array(list(string.ascii_letters + string.digits), dtype=(np.str_, 1))
     retval = (
         # TODO: remove the double unused-ignore-comment astral-sh/ty#2681
-        np.random.default_rng(2)  # ty: ignore[no-matching-overload,unused-ignore-comment,unused-ignore-comment]
+        np.random.default_rng(  # ty: ignore[no-matching-overload,unused-ignore-comment,unused-ignore-comment]
+            2
+        )
         .choice(chars, size=nchars * np.prod(size), replace=True)
         .view((np.str_, nchars))
         .reshape(size)
