24dabe9fac04bc1997687de966f421b2b9d57723
Loïc Diridollou <loic.diridollou@gmail.com>
2026-08-05 18:44:46 -0400

    Fix errors

--- pandas-stubs/core/frame.pyi
+++ pandas-stubs/core/frame.pyi
@@ -2282,6 +2282,7 @@ class DataFrame(NDFrame, OpsMixin, _GetItemHack):
     def pct_change(
         self,
         periods: int = 1,
+        fill_method: None = None,
         freq: Frequency | dt.timedelta | None = ...,
         fill_value: Scalar | NAType | None = ...,
         axis: Axis | None = ...,
