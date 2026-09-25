012ca32acf15c273acb5f9f21ec59767f6071082
Loïc Diridollou <loic.diridollou@gmail.com>
2026-09-24 20:13:31 -0400

    Formatting

--- tests/test_windowing.py
+++ tests/test_windowing.py
@@ -176,7 +176,7 @@ def test_rolling_aggregate() -> None:
     check(assert_type(DF.rolling(10).agg("sum"), DataFrame), DataFrame)
 
     if TYPE_CHECKING_INVALID_USAGE:
-        DF.rolling(10).aggregate("random_func")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType] # ty: ignore[no-matching-overload]  # pyrefly: ignore[no-matching-overload] # pyrefly: ignore[no-matching-overload]
+        DF.rolling(10).aggregate("random_func")  # type: ignore[arg-type] # pyright: ignore[reportArgumentType] # ty: ignore[no-matching-overload] # pyrefly: ignore[no-matching-overload] # pyrefly: ignore[no-matching-overload]
         DF.rolling(10).aggregate({"col1": "random_func"})  # type: ignore[dict-item] # pyright: ignore[reportArgumentType] # ty: ignore[no-matching-overload] # pyrefly: ignore[no-matching-overload]
 
 
